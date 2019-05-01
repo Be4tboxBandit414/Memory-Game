b3f28428c19c8d8179d2857c4a470174bb55cd73
Stanley Nguyen <stanley.nguyen@ruralsource.com>
2019-05-01 08:28:25 -0500

    second commit

--- card-match/src/components/header/Header.jsx
+++ card-match/src/components/header/Header.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Header = ({ restartGame }) => (
   <div className="grid-header-container">
-    <div className="justify-left timer" />
-    <div className="justify-center game-status-text" />
-    <div className="justify-end">
+    <div className="restart-container">
       <button onClick={restartGame} className="restart-button">
         Restart Game
       </button>
