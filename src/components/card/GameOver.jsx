2987194a50141115c346be53a536f465bc210589
Stanley Nguyen <stanley.nguyen@ruralsource.com>
2019-05-01 15:31:12 -0500

    styling change

--- src/components/card/GameOver.jsx
+++ src/components/card/GameOver.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
 //Stanley N: The Game Over Component that will display after user has completed the game with the ability to restart the game with the restartGame function.
-const GameOver = ({ restartGame }) => (
+const GameOver = () => (
   <div className="justify-center">
     <h1>Game Over!</h1>
     <h3>Thanks for playing!</h3>
-    <button className="restart-button" onClick={restartGame}>
-      Restart Game
-    </button>
   </div>
 );
 
