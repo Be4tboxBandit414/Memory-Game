c47eb523a94bca92ac1a6f3c748d546d6e7b67f7
Stanley Nguyen <stanley.nguyen@ruralsource.com>
2019-05-01 14:57:14 -0500

    Updates

---
+++ precache-manifest.aac82a9be9d832f35e3468e364f30150.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e72f26575dc6bcf885b50fe7ae52b6",
+    "url": "/Be4tboxBandit414/Memory-Game/index.html"
+  },
+  {
+    "revision": "3d6eb13e7c2303cc4857",
+    "url": "/Be4tboxBandit414/Memory-Game/static/css/main.8f628d9a.chunk.css"
+  },
+  {
+    "revision": "df39a5c65f0e1f833ea8",
+    "url": "/Be4tboxBandit414/Memory-Game/static/js/2.e7192084.chunk.js"
+  },
+  {
+    "revision": "3d6eb13e7c2303cc4857",
+    "url": "/Be4tboxBandit414/Memory-Game/static/js/main.b6c4e486.chunk.js"
+  },
+  {
+    "revision": "06bb7607af56e36db847",
+    "url": "/Be4tboxBandit414/Memory-Game/static/js/runtime~main.e847f355.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+
+importScripts(
+  "/Be4tboxBandit414/Memory-Game/precache-manifest.aac82a9be9d832f35e3468e364f30150.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Be4tboxBandit414/Memory-Game/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
